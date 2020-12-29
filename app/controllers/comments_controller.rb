0c112318e86d2419f4e5e46629cf26c725ab52a5
kiserukun <73691576+kiserukun@users.noreply.github.com>
2020-12-29 16:32:29 +0900

    コントローラーに定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,11 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(comment)
+    redirect_to "/items/#{comment.item.id}"
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+    end
 end
